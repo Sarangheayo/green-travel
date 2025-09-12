e14addbefec99efb80708eedcb1e7f2190f17cc8
Sarangheayo <myungsoo8504@gmail.com>
2025-09-12 11:13:28 +0900

    250912 router setting

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import './App.css';
 import Header from './components/common/Header.jsx';
+import { Outlet } from 'react-router-dom';
 
 function App() {
     return (
       <>
        <Header></Header>
+       <main>
+        <Outlet />
+       </main>
       </>
-
   )
-
 }
 
 export default App;
