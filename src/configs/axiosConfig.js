456230842cd67260578b8c6ed0a838d4f5f113cd
Sarangheayo <myungsoo8504@gmail.com>
2025-09-12 17:32:49 +0900

    250912 FESTIVAL_LIST01 axios 처리까지 완료, 추후 렌더링 필요

---
+++ src/configs/axiosConfig.js
@@ -0,0 +1,10 @@
+const axiosConfig = {
+  serviceKey: 'e36f981d63b9ef7a5c198f058a3c7c0b489949c429bd9da5169c1662b310a2e6', 
+  MobileOS: 'WEB',
+  MobileApp: 'Green Travel',
+  type: 'json',
+  arrange: 'O',
+  baseUrl: 'https://apis.data.go.kr/B551011/KorService2',
+}
+
+export default axiosConfig;
