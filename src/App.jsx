690e86f1ac65cac9b8c1571df48a467b46e31f70
Sarangheayo <myungsoo8504@gmail.com>
2025-09-18 17:29:22 +0900

    250918 installPrompt_IOS

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Outlet, ScrollRestoration } from 'react-router-dom';
 import { localStorageUtil } from './utils/localStorageUtil.js';
 import { dateFormatter } from './utils/dateFormatter.js';
 import BeforeInstallPrompt from './components/BeforeInstallPrompt.jsx';
+import InstallPrompt from './components/InstallPrompt.jsx';
 
 
 function App() {
@@ -30,6 +31,7 @@ function App() {
 
     return (
       <>
+       <InstallPrompt />
        <BeforeInstallPrompt />
        <Header></Header>
        <main>
